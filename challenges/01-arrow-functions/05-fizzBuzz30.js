96767ddd7e0fda8e4a3120f883b48b1e8cbd0019
Nathan Vu <nathan.vu5@gmail.com>
2019-09-18 16:23:49 -0700

    1 out of 3 passed

--- challenges/01-arrow-functions/05-fizzBuzz30.js
+++ challenges/01-arrow-functions/05-fizzBuzz30.js
@@ -2,13 +2,22 @@
 //1 through 30 replacing numbers divisible by 3 with 'fizz', numbers divisible by 5 with 'buzz' and numbers divisible by 3 and 5 with
 //'fizzbuzz'
 
-let fizzBuzz30;
+// let fizzBuzz30;
 
 
+const fizzBuzz30 = () =>
+{
+let arr = [];
+for (i = 0; i < 30; i++) {
+  if(i%15==0) console.log("FizzBuzz");
+  else if (i % 3 == 0) arr.push("Fizz");
+  else if (i & 5 == 0) arr.push("Buzz");
+  else arr.push(i);
+}
 
+}
 
-
-
+fizzBuzz30(1);
 
 
 //Do not edit
